39a82c79f8c77a7154add77ddb5f80840f613032
unknown <varghdyra@gmail.com>
2019-09-26 21:43:03 +0200

    first commit

---
+++ Assets/Moment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moment : MonoBehaviour {
+
+    public Controlls controller;
+  
+  public float runSpeed = 40f;
+
+  float horizontalMove = 10f;
+
+    // Update is called once per frame
+    void Update() {
+   
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; 
+    
+    }
+    
+    //move my character
+    void FixedUpdate ()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
